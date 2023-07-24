770ab20eb2b2f863cf7a898ba03e4b8636410748
Taya Simakova <stipaxa@gmail.com>
2023-07-24 21:23:14 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import TheSignInForm from './components/TheSignInForm.vue'
+import TheSignUpForm from './components/TheSignUpForm.vue'
+</script>
+
+<template>
+    <div class="center">
+        <div class="box">
+            <TheSignInForm />
+        </div>
+    </div>
+    <div class="center">
+        <div class="box">
+            <TheSignUpForm />
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.center {
+    display: flex;
+    justify-content: center;
+}
+.box {
+    width: 60%;
+    box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.25);
+    padding: 1rem;
+    background-color: white;
+    border-radius: 5px;
+    margin-bottom: 1rem;
+}
+</style>
---
+++ src/components/TheSignInForm.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+        <div>
+            <div class="labels"><label>Username or email </label></div>
+            <div class="input-container">
+                <input type="text" placeholder="login" />
+            </div>
+            <div class="labels"><label>Password</label></div>
+            <div class="input-container">
+                <input type="text" placeholder="password" />
+            </div>
+            <div class="btn-send">
+                <button>Submit</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.input-container {
+    display: flex;
+}
+.btn-send {
+    margin-top: 0.5rem;
+    text-align: center;
+}
+
+.labels {
+    margin-bottom: 0.2rem;
+}
+
+input {
+    border: 2px solid rgba(67, 67, 68, 0.599);
+    border-radius: 5px;
+    flex-grow: 1;
+    padding: 0.5rem;
+    margin-bottom: 0.5rem;
+}
+
+input:focus {
+    outline: none;
+    border: 1px solid rgb(220, 31, 198);
+}
+
+button {
+    border: none;
+    background-color: rgb(105, 105, 106);
+    border-radius: 5px;
+    color: aliceblue;
+    padding: 0.5rem 1rem;
+    cursor: pointer;
+}
+
+button:hover {
+    background-color: rgb(220, 31, 198);
+    border-color: rgb(220, 31, 198);
+}
+</style>
